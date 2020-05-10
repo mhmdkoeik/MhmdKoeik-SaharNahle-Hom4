d769075c14f99d2609a4060e23d3d970e3c8cdde
mhmdkoeik <44830755+mhmdkoeik@users.noreply.github.com>
2020-05-10 21:07:05 +0300

    changes

---
+++ MhmdKoeik-HomeWork3/Migrations/202005101806333_transferTable.cs
@@ -0,0 +1,16 @@
+namespace MhmdKoeik_HomeWork3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class transferTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ MhmdKoeik-HomeWork3/Models/Transfer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MhmdKoeik_HomeWork3.Models
+{
+    public class Transfer
+    {
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int CheckingAccountId { get; set; }
+
+        [Required]
+        [Display(Name = "To Account #")]
+        public string TransactionSource { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public string TransactionDate{get; set;}
+    }
+}
