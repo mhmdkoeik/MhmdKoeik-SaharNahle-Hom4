8eef2c3f189fbebb6743a5c9ff2fe848c782bd92
mostafa <mostafatamimi@gmail.com>
2020-05-13 14:15:56 +0300

    test

---
+++ MhmdKoeik-HomeWork3/Models/MustafaTamimiModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MhmdKoeik_HomeWork3.Models
+{
+    public class MustafaTamimiModel
+    {
+        public int HelloTHisIsTest { get; set; }
+    }
+}
